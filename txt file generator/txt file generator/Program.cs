91347386aed84a626394a97c0a68949e24d98f42
fkotik <96012122+fkotik@users.noreply.github.com>
2023-05-05 15:48:09 +0400

    Add files via upload

---
+++ txt file generator/txt file generator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+Console.WriteLine("Укажите путь к папке где будут генерироваться файлы");
+string path2 = Console.ReadLine();
+Console.WriteLine("Укажите минимальное кол-во слов в файле");
+int min = int.Parse(Console.ReadLine());
+Console.WriteLine("Укажите максимальное кол-во слов в файле");
+int max = int.Parse(Console.ReadLine());
+Console.WriteLine("Укажите кол-во файлов которые надо сгенерировать");
+int t = int.Parse(Console.ReadLine());
+Console.WriteLine("Укажите путь к файлу от куда будет браться список слов");
+string path = Console.ReadLine();
+
+string[] slovar = File.ReadAllLines(@path);
+
+var rnd = new Random();
+
+string[] znaki = {" ", ", ", ". "};
+int j = 1;
+while (t-->0)
+{
+    string text="";
+    int i = rnd.Next(min, max+1);
+    while (i-- > 0)
+    {
+        text += slovar[rnd.Next(0, slovar.Length)] + znaki[rnd.Next(0, znaki.Length)];
+    }
+    string path3 = path2 + j + ".txt";
+    j++;
+    File.WriteAllText(@path3, text);
+}
